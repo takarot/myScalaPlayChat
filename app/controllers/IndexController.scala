9a24d3a13ed10b796f4e45959f7b39f57bccd243
takarot <xeno.pc@outlook.com>
2017-07-04 21:36:06 +0900

    Initial commit

---
+++ app/controllers/IndexController.scala
@@ -0,0 +1,60 @@
+package controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import daos.TodoDao
+import javax.inject.{Inject, Singleton}
+
+import models.Todo
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller}
+
+case class TodoForm(action: String, content: String)
+
+@Singleton
+class IndexController @Inject() (val todoDao: TodoDao, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val todoForm = Form(
+    mapping(
+      "action" -> text,
+      "content" -> text
+    )(TodoForm.apply)(TodoForm.unapply))
+
+  /**
+    * 初期表示(GET)
+    */
+  def get = Action.async {
+    todoDao.all().map(todos => Ok(views.html.index(todoForm, todos)))
+  }
+
+  /**
+    * アクション(POST)
+    */
+  def post = Action.async { implicit request =>
+    todoForm.bindFromRequest.fold(
+      formWithErrors => {
+        todoDao.all().map(todos => Ok(views.html.index(formWithErrors, todos)))
+      },
+      todoData => {
+        val action = todoData.action.split(":")
+        action(0) match {
+          case "insert" => insert(todoData)
+          case "update" => update(action(1), todoData)
+          case "delete" => delete(action(1))
+          case _ => println("No Action!!")
+        }
+        todoDao.all().map(todos => Ok(views.html.index(todoForm.fill(todoData), todos)))
+      })
+  }
+
+  /** 登録 */
+  def insert(todoForm: TodoForm) = todoDao.insert(Todo(0, todoForm.content))
+
+  /** 更新 */
+  def update(id: String, todoForm: TodoForm) = todoDao.update(Todo(id.toLong, todoForm.content))
+
+  /** 削除 */
+  def delete(id: String) = todoDao.delete(Todo(id.toLong, ""))
+
+}
---
+++ app/models/Todo.scala
@@ -0,0 +1,6 @@
+package models
+
+/**
+  * Created by utsumi on 17/07/04.
+  */
+case class Todo(id: Long, content: String) {}
